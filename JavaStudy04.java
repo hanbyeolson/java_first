697c517d84f048eb187c1d1cc0e3939fdaa17cca
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-08-15 21:52:47 +0900

    Update JavaStudy04.java

--- JavaStudy04.java
+++ JavaStudy04.java
@@ -12,31 +12,65 @@ public class 미니과제04 {
 
         Scanner sc = new Scanner(System.in);
 
-
-
-        System.out.print("출생년도를 입력해 주세요.(yyyy):");
-        int year = sc.nextInt();
-
-        System.out.print("출생월을 입력해 주세요.(mm):");
-        int month = sc.nextInt();
-
-        System.out.print("출생일을 입력해 주세요.(dd):");
-        int day= sc.nextInt();
-
-        System.out.print("성별을 입력해 주세요.(m/f):");
-        String gender = String.valueOf(sc.nextLine());
-
-
-
         String str;
         str = sc.nextLine();
+        int gendernum = 0;
 
-        if (gender.equals("f")) {
-            gender = "4";
-        } else if (gender.equals("m")) {
-            gender = "3";
+        if (gendernum == 'f') {
+            gendernum = 4;
+        } else if (gendernum == 'm') {
+            gendernum = 3;
         }
 
+        int year;
+        int month;
+        int day;
+        String gender;
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.toString(year))
+                .append(month >= 10? month : String.format("%02d", month))
+                .append(day >= 10 ? day : String.format("%02d", day))
+                .append("-")
+                .append(gendernum)
+                .append(new Random().nextInt(1000000));
+
+            Scanner kb = new Scanner(System.in);
+
+            while(true) {
+                System.out.print("출생년도를 입력해 주세요.(yyyy):");
+                year = kb.nextInt();
+
+                if(year < 1000 || year > 2022) {
+                    System.out.println("출생년도 입력이 잘못되었습니다. 다시 시작합니다.\n");
+                    continue;
+                }
+                System.out.print("출생월을 입력해 주세요.(mm):");
+                month = kb.nextInt();
+            
+                if(month <= 0 || month > 12) {
+                    System.out.println("출생월 입력이 잘못되었습니다. 다시 시작합니다.\n");
+                    continue;
+                }
+
+                System.out.print("출생일을 입력해 주세요.(dd):");
+                day = kb.nextInt();
+                
+                if(day <= 0 || day > 31) {
+                    System.out.println("출생일 입력이 잘못되었습니다. 다시 시작합니다.\n");
+                    continue;
+                }
+                System.out.print("성별을 입력해 주세요.(m/f):");
+                gender = String.valueOf(Character.toLowerCase(kb.next().charAt(0)));
+               
+                
+                if(gender == "m" && gender == "f") {
+                    System.out.println("성별 입력이 잘못되었습니다. 다시 시작합니다.\n");
+                    continue;
+                }
+
+                break;
+            }
         int num = 0;
         String randomNum = "";
 
@@ -46,10 +80,6 @@ public class 미니과제04 {
             num = random.nextInt(9);
             randomNum += Integer.toString(num);
         }
-
-
         System.out.printf("%s%02d%02d-%d%s",year,month,day,gender,randomNum);
-
-
+        }
     }
-}
